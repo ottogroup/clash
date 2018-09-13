3869b310c54d4126ab279692d56751201c351ef4
Mike Czech <mike.czech@ottogroup.com>
2018-09-13 18:16:33 +0200

    Add task to run clash from the commandline

--- python/setup.py
+++ python/setup.py
@@ -1,10 +1,24 @@
+import os
 from setuptools import setup, find_packages
 
-setup(
-    name="pyclash",
-    version="0.0.1",
-    author="BIWS",
-    description="Simple jobs for the Google Compute Engine",
-    packages=find_packages(),
-    install_requires=[],
-)
+version = open('../VERSION').read().rstrip()
+
+install_requires = []
+
+tests_require = ['pytest']
+
+setup(name='clash',
+      version=version,
+      description='Running bash scripts on the Google Compute Engine',
+      author='Otto Group',
+      license='Apache License, Version 2.0',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      entry_points={
+          'console_scripts': [
+              'clash = pyclash.clash:main',
+              ]
+          },
+      )
