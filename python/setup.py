ff93861ff9cc8c3281aaaf1324a422cf78ff6b36
Mike Czech <mike.czech@ottogroup.com>
2018-09-14 14:24:56 +0200

    Add command line client

--- python/setup.py
+++ python/setup.py
@@ -1,24 +1,21 @@
 import os
 from setuptools import setup, find_packages
 
-version = open('../VERSION').read().rstrip()
+version = open("../VERSION").read().rstrip()
 
 install_requires = []
 
-tests_require = ['pytest']
+tests_require = ["pytest"]
 
-setup(name='clash',
-      version=version,
-      description='Running bash scripts on the Google Compute Engine',
-      author='Otto Group',
-      license='Apache License, Version 2.0',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=install_requires,
-      entry_points={
-          'console_scripts': [
-              'clash = pyclash.clash:cli',
-              ]
-          },
-      )
+setup(
+    name="clash",
+    version=version,
+    description="Running bash scripts on the Google Compute Engine",
+    author="Otto Group",
+    license="Apache License, Version 2.0",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={"console_scripts": ["clash = pyclash.clash:cli"]},
+)
