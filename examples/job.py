a064901873a2e660410eb293de84abe52baea412
Mike Czech <mike.czech@ottogroup.com>
2018-12-21 12:50:21 +0100

    Improve example for "user journey"

--- examples/job.py
+++ examples/job.py
@@ -4,14 +4,22 @@ from pyclash.clash import JobConfigBuilder
 DEFAULT_JOB_CONFIG = (
     JobConfigBuilder()
     .project_id("***REMOVED***")
-    .image("google/cloud-sdk")
+    .image("eu.gcr.io/***REMOVED***/***REMOVED***:latest")
     .machine_type("n1-standard-1")
     .privileged(True)
     .build()
 )
 
+gcs_target = {
+    "/tmp/artifacts": "test-bucket-4532"
+}
+
+gcs_mounts = {
+    "test-bucket-4532": "/home/app/mnt/bucket"
+}
+
 job = clash.Job(job_config=DEFAULT_JOB_CONFIG, name_prefix="test")
-job.run("echo hello")
+job.run("echo hello && touch /tmp/artifacts/woo.txt && ls -l /home/app/mnt/bucket", gcs_target=gcs_target, gcs_mounts=gcs_mounts)
 
 result = job.attach()
 if result["status"] != 0:
