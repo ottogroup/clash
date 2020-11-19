cdbec10c17906e8b83db566ec4ce8a6f3a59e1eb
Mike Czech <mike.czech@ottogroup.com>
2020-11-19 18:13:47 +0100

    Add CLI

---
+++ python/pyclash/cli.py
@@ -0,0 +1,36 @@
+""" CLI for Clash """
+
+import sys
+
+import click
+
+from pyclash.clash import JobConfigBuilder, Job
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option("--name", type=click.STRING, required=True)
+@click.option("--project", type=click.STRING, required=True)
+@click.option("--image", type=click.STRING, required=True)
+@click.option("--subnetwork", type=click.STRING, required=True)
+@click.option("--preemptible", type=click.BOOL, required=False, default=False)
+@click.option(
+    "--machine-type", type=click.STRING, default="n1-standard-1", required=False
+)
+@click.option("--arg", type=click.STRING, required=True, multiple=True)
+def run(name, project, image, subnetwork, preemptible, machine_type, arg):
+    config = (
+        JobConfigBuilder()
+        .project_id(project)
+        .image(image)
+        .machine_type(machine_type)
+        .subnetwork(subnetwork)
+        .preemptible(preemptible)
+        .build()
+    )
+    result = Job(job_config=config, name_prefix=name).run(arg, wait_for_result=True)
+    sys.exit(result["status"])
