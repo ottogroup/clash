3fc3443d860d57f3506b20cfeee2f42c877bf8c2
Mike Czech <mike.czech@ottogroup.com>
2020-11-24 14:49:24 +0100

    Add oauth scopes

--- python/pyclash/clash.py
+++ python/pyclash/clash.py
@@ -37,6 +37,8 @@ DEFAULT_JOB_CONFIG = {
         "https://www.googleapis.com/auth/logging.write",
         "https://www.googleapis.com/auth/monitoring",
         "https://www.googleapis.com/auth/pubsub",
+        "https://www.googleapis.com/auth/cloud-platform",
+        "https://www.googleapis.com/auth/cloudplatformprojects"
     ],
     "allowed_persistence_regions": [
         "europe-north1",
