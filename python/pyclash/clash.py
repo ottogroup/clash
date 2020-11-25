eaf2bd82e2f0fd0cd10e56ed49bb0b9fecdc372c
Mike Czech <mike.czech@ottogroup.com>
2020-11-25 16:52:21 +0100

    Use stronger machine type by default

--- python/pyclash/clash.py
+++ python/pyclash/clash.py
@@ -26,7 +26,7 @@ DEFAULT_JOB_CONFIG = {
     "zone": "europe-west1-b",
     "region": "europe-west1",
     "subnetwork": "default-europe-west1",
-    "machine_type": "n1-standard-1",
+    "machine_type": "n1-standard-4",
     "service_account": "default",
     "disk_image": {"project": "gce-uefi-images", "family": "cos-stable"},
     "scopes": [
